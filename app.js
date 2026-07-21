eaee501fbbb05b1aa75ff5d6fd923821b9099017
aditya-4545 <akale4818@gmail.com>
2026-07-21 23:54:53 +0530

    Fix route Vercel

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const PORT = process.env.PORT || 8080;
 
 if (require.main === module) {
     app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
+        console.log(`Server running on http://localhost:${PORT}/posts`);
     });
 }
 
